fd1cdf9a5b85a35060a258f2823c1c428138acb0
Pratham <chudasamapratham142@gmail.com>
2024-11-13 00:28:22 +0530

    changing the text

--- src/components/global/footer.tsx
+++ src/components/global/footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="fixed bottom-0 left-1/2 mb-6 w-full -translate-x-1/2 text-center text-[14px]">
-      by submitting you agree to our{" "}
+      by using our app you agree to our{" "}
       <a
         href="https://github.com/chain-labs/pulse-fe/blob/main/terms-conditions.md"
         target="_blank"
